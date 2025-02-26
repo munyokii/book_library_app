af966da67e1a73d7932e584829ad10bfe93b8db8
Muli Job <munyoki912@gmail.com>
2025-02-26 12:51:30 +0300

    added functionality to get all users views a borrower detail finding and creating a borrower

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -1,7 +1,29 @@
 class BorrowersController < ApplicationController
   def index
+    @borrowers = Borrower.all
   end
 
   def show
+    @borrower = Borrower.find(params[:id])
+    @loans = @borrower.loans.includes(:book).order(borrowed_date: :desc)
+  end
+  
+  def find_or_create
+    @borrower = Borrower.find_by(name: borrower_params[:name])
+    
+    if @borrower.nil?
+      @borrower = Borrower.new(borrower_params)
+      unless @borrower.save
+        return render json: { errors: @borrower.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+    
+    render json: { id: @borrower.id, name: @borrower.name }
+  end
+  
+  private
+  
+  def borrower_params
+    params.require(:borrower).permit(:name, :email, :phone)
   end
 end
