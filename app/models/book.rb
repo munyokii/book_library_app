e86aab42c2041d3b1529689f5b6cd55fca8c6355
Muli Job <munyoki912@gmail.com>
2025-02-26 12:02:18 +0300

    Added validation to book model fields and relations to check loaned book which haven't been returned, checking which books are currently on loan and updates available status based on loan status

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,25 @@
 class Book < ApplicationRecord
+  has_many :loans, dependent: :destroy
+  has_many :borrowers, through: :loans
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :publication_year, numericality: { 
+    allow_nil: true,
+    only_integer: true, 
+    greater_than_or_equal_to: 1450, 
+    less_than_or_equal_to: -> { Date.current.year } 
+  }
+  
+  def current_loan
+    loans.where(returned_date: nil).first
+  end
+  
+  def on_loan?
+    current_loan.present?
+  end
+  
+  def update_availability
+    update(available: !on_loan?)
+  end
 end
