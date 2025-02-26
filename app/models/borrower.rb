a9ab221616b14a0bd65bbeed10647a65c9d299cf
Muli Job <munyoki912@gmail.com>
2025-02-26 12:04:16 +0300

    set up borrower model relations and validation for email

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -1,2 +1,15 @@
 class Borrower < ApplicationRecord
+  has_many :loans, dependent: :destroy
+  has_many :books, through: :loans
+  
+  validates :name, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, allow_blank: true
+  
+  def current_loans
+    loans.where(returned_date: nil)
+  end
+  
+  def borrowed_books
+    books.distinct
+  end
 end
